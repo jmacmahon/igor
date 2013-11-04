072dfc9ed1ba99a6858aa0e8312a8c03e1fe0c46
Sam Clements <sam@borntyping.co.uk>
2013-11-04 21:00:16 +0000

    Command Listeners

--- igor/__init__.py
+++ igor/__init__.py
@@ -7,8 +7,13 @@ import select
 import os
 
 from igor.irc.connection import Connection
+from igor.irc.messages import Message
 from igor.reactor import Reactor
 from igor.plugins import TestPlugin
+
+def test():
+    plugin = TestPlugin()
+    plugin(Message.parse(":npc!npc@borntyping.co.uk PRIVMSG #69 ::test some arguments"))
 
 def main():
     Reactor([Connection("irc.aberwiki.org", 6667)], [TestPlugin()]).go()
--- igor/plugins/__init__.py
+++ igor/plugins/__init__.py
@@ -29,25 +29,48 @@ class Listener(object):
     def __get__(self, instance, owner):
         return BoundListener(self, instance)
 
-    def __set__(self, function):
-        self.function = function
-
     def __call__(self, instance, message):
         if self.predicate is None or self.predicate(message):
             return self.function(instance, message)
 
-def listener(*args, **kwargs):
-    """Returns a decorator function that will create a listener instance"""
+class Command(Listener):
+    COMMAND_CHARACTER = ':'
 
-    # If the only argument passed is a function, create a Listener
-    if len(args) == 1 and callable(args[0]) and not kwargs:
-        return Listener(function=args[0])
+    def __init__(self, function, command):
+        self.function = function
+        self.command = command
 
-    # This decorator will create a Listener with the arguments we already have
-    def create_listener(function):
-        return Listener(function, *args, **kwargs)
+    def __call__(self, instance, message):
+        if isinstance(message, Privmsg) and message.trailing.startswith(self._prefix):
+            return self.function(instance, message, self._arguments(message))
 
-    return create_listener
+    @property
+    def _prefix(self):
+        return self.COMMAND_CHARACTER + self.command
+
+    def _arguments(self, message):
+        arguments = message.trailing.split(' ', 1)
+        return arguments[1] if arguments[1] else ''
+
+class ListenerDecorator(object):
+    def __init__(self, listener_class, ):
+        self.listener_class = listener_class
+
+    def __call__(self, *args, **kwargs):
+        """Returns a decorator function that will create a listener instance"""
+
+        # If the only argument passed is a function, create a Listener
+        if len(args) == 1 and callable(args[0]) and not kwargs:
+            return self.listener_class(function=args[0])
+
+        # This decorator will create a Listener with the arguments we already have
+        def create_listener(function):
+            return self.listener_class(function, *args, **kwargs)
+
+        return create_listener
+
+listener = ListenerDecorator(Listener)
+command = ListenerDecorator(Command)
 
 class Plugin(object):
     def __call__(self, message):
@@ -55,13 +78,13 @@ class Plugin(object):
             try:
                 listener(message)
             except Exception as exception:
+                print("Listener {} failed to run".format(name))
                 self.__log_exception(exception)        
 
     def __is_listener(self, object):
         return isinstance(object, BoundListener)
 
     def __log_exception(self, exception):
-        print("Listener {} failed to run".format(name), file=sys.stderr)
         print(traceback.format_exc(exception), file=sys.stderr)
 
 class TestPlugin(Plugin):
@@ -72,3 +95,7 @@ class TestPlugin(Plugin):
     @listener
     def test_all(self, message):
         print(message)
+
+    @command("test")
+    def test_command(self, message, arguments):
+        print("Called command test with", repr(arguments))
