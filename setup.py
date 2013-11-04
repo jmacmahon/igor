6b557ed4a366c2c88e19e966501ddcdb4f68dd5b
Sam Clements <sam@borntyping.co.uk>
2013-11-04 16:17:29 +0000

    Added package metadata

---
+++ setup.py
@@ -0,0 +1,39 @@
+"""Package metadata"""
+
+from __future__ import unicode_literals
+
+import setuptools
+
+setuptools.setup(
+    name = "igor",
+    version = "0.1.0",
+    url = "https://github.com/borntyping/igor",
+
+    author = "Sam Clements",
+    author_email = "sam@borntyping.co.uk",
+
+    description = "An IRC bot reanimated from various other bots",
+    long_description = open('README.md').read(),
+
+    packages = setuptools.find_packages(),
+
+    entry_points = {
+        'console_scripts': [
+            'igor = igor:main'
+        ]
+    },
+
+    classifiers = [
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'License :: OSI Approved',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: Unix',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Communications',
+        'Topic :: Communications :: Chat',
+        'Topic :: Communications :: Chat :: Internet Relay Chat',
+    ],
+)
